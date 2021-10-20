c8985b06acd1c4e1aa340571070203f1d57800cb
Rodel Letran <rodel.letran@sun-aterisk.com>
2021-10-20 18:26:27 +0800

    [TASK] feature/quiz

--- src/sels_fe/src/store/reducers/index.js
+++ src/sels_fe/src/store/reducers/index.js
@@ -2,22 +2,24 @@ import { combineReducers } from "redux"
 
 import { signinReducer } from "./user-signin"
 import { signupReducer } from "./user-signup"
-import { gettUsersReducer } from "./get-user-list"
 import { getAuthUserDetailsReducer } from "./get-auth-user-details"
 import { getFollowedUsersReducer } from "./get-followed-users"
 import { quizReducer } from "./quiz"
 import { questionReducer } from "./question"
 import { choiceReducer } from "./choice"
+import { answerReducer } from "./answer"
+import { userReducer } from "./user"
 
 const reducers = combineReducers({
   Signup: signupReducer,
   Signin: signinReducer,
-  Users: gettUsersReducer,
   AuthUser: getAuthUserDetailsReducer,
   FollowedUsers: getFollowedUsersReducer,
   Quiz: quizReducer,
   Question: questionReducer,
   Choice: choiceReducer,
+  Answer: answerReducer,
+  User: userReducer,
 })
 
 export default reducers
