e2d66306a8757dc08760400e6d79680010a003a6
Rodel Letran <rodel.letran@sun-aterisk.com>
2021-10-21 16:37:38 +0800

    [ADD] bulk-create-api-endpoints

--- src/answers_choices/urls.py
+++ src/answers_choices/urls.py
@@ -3,10 +3,10 @@ from django.urls.conf import include
 
 from rest_framework import routers
 
-from .views import ( 
-  AnswerViewSet,
-  ChoiceViewSet,
-  QuestionViewSet
+from .views import (
+    AnswerViewSet,
+    ChoiceViewSet,
+    QuestionViewSet
 )
 
 router = routers.SimpleRouter()
@@ -15,8 +15,17 @@ router.register(r'choice', ChoiceViewSet)
 router.register(r'question', QuestionViewSet)
 
 
-app_name='answer_choice'
+app_name = 'answer_choice'
 
 urlpatterns = [
-  path('', include(router.urls)),
+    path('', include(router.urls)),
+    path('questions/bulk',
+         QuestionViewSet.as_view({'post': 'bulk_create'}),
+         name='create-questions'),
+    path('choices/bulk',
+         ChoiceViewSet.as_view({'post': 'bulk_create'}),
+         name='create-choices'),
+    path('answers/bulk',
+         AnswerViewSet.as_view({'post': 'bulk_create'}),
+         name='create-answers'),
 ]
--- src/answers_choices/views.py
+++ src/answers_choices/views.py
@@ -2,31 +2,53 @@ from django.contrib.auth import get_user_model
 
 
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
 
 from .serializers import (
-  AnswerSerializer,
-  ChoiceSerializer,
-  QuestionSerializer
+    AnswerSerializer,
+    ChoiceSerializer,
+    QuestionSerializer
 )
 from .models import Answer, Choice, Question
 
 
-
-
-
 class AnswerViewSet(viewsets.ModelViewSet):
-  queryset = Answer.objects.all()
-  serializer_class = AnswerSerializer
-  # permission_classes = [ IsAuthenticated ]
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    # permission_classes = [ IsAuthenticated ]
+
+    @action(methods=['POST'], detail=False)
+    def bulk_create(self, request):
+        serializer = AnswerSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class ChoiceViewSet(viewsets.ModelViewSet):
-  queryset = Choice.objects.all()
-  serializer_class = ChoiceSerializer
-  # permission_classes = [ IsAuthenticated ]
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+    # permission_classes = [ IsAuthenticated ]
+
+    @action(methods=['POST'], detail=False)
+    def bulk_create(self, request):
+        serializer = ChoiceSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class QuestionViewSet(viewsets.ModelViewSet):
-  queryset = Question.objects.all()
-  serializer_class = QuestionSerializer
-  # permission_classes = [ IsAuthenticated ]
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    # permission_classes = [ IsAuthenticated ]
 
-
+    @action(methods=['POST'], detail=False)
+    def bulk_create(self, request):
+        serializer = QuestionSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
