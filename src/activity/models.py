2ecc3d2c4037bff8e838a0a7508bbc1d64da44b4
Rodel Letran <rodel.letran@sun-aterisk.com>
2021-10-28 18:00:59 +0800

    [FIX] remove/unnecessary-comments

--- src/activity/models.py
+++ src/activity/models.py
@@ -1,39 +1,3 @@
-# from django.db import models
-# from django.utils.translation import ugettext_lazy as _
-# from django.contrib.auth import get_user_model
-
-
-# USER = get_user_model()
-# FOLLOW = 'lesson.LESSON'
-# QUIZ = 'quiz.QUIZ'
-
-
-# class Activity(models.Model):
-#     class TYPE(models.TextChoices):
-#         FOLLOWED = 'Following',
-#         TAKEN = 'Take a Lesson'
-
-#     user = models.ForeignKey(USER, default=1, on_delete=models.CASCADE)
-#     activity_type = models.CharField(
-#         _("activity type"), max_length=50,  choices=TYPE.choices, blank=True, null=True)
-
-#     followed_user = models.ForeignKey(
-#         USER, related_name="end_user", blank=True, null=True, on_delete=models.CASCADE)
-#     started_lesson = models.ForeignKey(
-#         QUIZ, blank=True, null=True, on_delete=models.CASCADE)
-#     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
-#     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
-
-#     class Meta:
-#         ordering = ['-pk']
-#         unique_together = [['user', 'followed_user']]
-#         verbose_name = 'Activity'
-#         verbose_name_plural = 'Activities'
-
-#     def __str__(self):
-#         return f'{self.activity_type}'
-
-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import get_user_model
