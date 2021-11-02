9f07ffb31a06aa28bdcd30f7586a1e6eb64f1c45
Rodel Letran <rodel.letran@sun-aterisk.com>
2021-11-02 21:32:01 +0800

    [FIx] initial-setup

--- src/sels_fe/src/store/reducers/index.js
+++ src/sels_fe/src/store/reducers/index.js
@@ -13,7 +13,10 @@ import { lessonReducer } from "./lesson"
 import { answeredReducer } from "./answered"
 import { activityReducer } from "./activity"
 
-const reducers = combineReducers({
+// const RESET_REDUX_STATE = "RESET_REDUX_STATE"
+import { AUTH_LOGOUT } from "../actions/UserSignin/action-types"
+
+const combinedReducer = combineReducers({
   Signup: signupReducer,
   Signin: signinReducer,
   AuthUser: getAuthUserDetailsReducer,
@@ -27,5 +30,26 @@ const reducers = combineReducers({
   Answered: answeredReducer,
   Activity: activityReducer,
 })
+
+const reducers = (state, action) => {
+  switch (action.type) {
+    case AUTH_LOGOUT:
+      delete state.Signup
+      delete state.Signin
+      delete state.AuthUser
+      delete state.FollowedUsers
+      delete state.Quiz
+      delete state.Question
+      delete state.Choice
+      delete state.Answer
+      delete state.User
+      delete state.Lesson
+      delete state.Answered
+      delete state.Activity
+      break
+    default:
+      return combinedReducer(state, action)
+  }
+}
 
 export default reducers
